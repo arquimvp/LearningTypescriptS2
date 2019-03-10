d5d5e95dc66a65faa72ff54d457f4dc54cbab981
arquimvp <alberto.martinez@kairosds.com>
2019-03-10 11:25:56 -0600

    master - challenge(app): Codigo JS, redefinir en typescript

--- app.ts
+++ app.ts
@@ -1,26 +1,56 @@
-/**
- * Typeof
- * Sirve para revisar el tipo de una variable u objeto
- */
+//#1
+//Crear un tipo apartir del cual pueda utilizar en los 2 objetos siguientes:
+// Objetos:
 
- let algo : any = 7;
+var patriot4x4 = {
+  color: "Negra",
+  modelo: "4x4",
+  blindada: true
+};
 
- console.log(typeof algo);  //number
+var lobo4x4 = {
+  color: "Negra",
+  modelo: "Harley Davidson",
+  blindada: true,
+  rodar(){ // El metodo rodar es opcional
+    console.log("rodando");
+  }
+};
 
 
- let otroAlgo : any = 'hola';
+//#2
+//Crear un tipo que pueda utilizar para el siguiente objeto.
+// desarrolladores debe de ser un arreglo de objetos personalizados.
+var desarrolladores = [{
+  perfil:"IOS",
+  edad: 23,
+  vidaSocial:false
+},{
+  perfil:"ANDROID",
+  edad: 25,
+  vidaSocial:true
+},{
+  perfil:"IONIC",
+  edad: 21,
+  vidaSocial:false
+}];
 
- console.log(typeof otroAlgo); //string
 
+//#3
+// Multiples tipos
+// cree dos tipos, uno para batman y otro para superman
+var batman = {
+  poder:"millonario",
+  estatura: 1.78
+};
 
- let obj : any = {};
+var superman = {
+  lider:true,
+  poderes: ["volar","vision calorifica","velocidad", "aliento helado"]
+}
 
- console.log(typeof obj); //object
+// superhybrid, debe poder ser cualquiera de los dos superheroes (batman y superman)
+var superHybrid;
 
-
- let func : any = function(){
-   return null;
- }
-
- console.log(typeof func); //function
-
+superHybrid = batman;
+superHybrid = superman;
